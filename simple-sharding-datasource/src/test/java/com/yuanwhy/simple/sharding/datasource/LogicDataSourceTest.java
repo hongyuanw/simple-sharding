d9bcf158e61a0c36b300ef116ab791c45ac31f62
yuanwhy <henrywangwhy@gmail.com>
2017-03-28 00:37:15 +0800

    move file

--- simple-sharding-datasource/src/test/java/com/yuanwhy/simple/sharding/datasource/LogicDataSourceTest.java
+++ simple-sharding-datasource/src/test/java/com/yuanwhy/simple/sharding/datasource/LogicDataSourceTest.java
@@ -1,11 +1,9 @@
 package com.yuanwhy.simple.sharding.datasource;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
-import java.util.Map;
 
 /**
  * Created by yuanwhy on 17/3/27.
