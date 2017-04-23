80d6a40c1af70e162173ea15f46d1cca437ea280
yuanwhy <henrywangwhy@gmail.com>
2017-04-23 12:48:54 +0800

    will support single table

---
+++ src/test/java/com/yuanwhy/simple/sharding/jdbc/SingleTableTest.java
@@ -0,0 +1,7 @@
+package com.yuanwhy.simple.sharding.jdbc;
+
+/**
+ * Created by yuanwhy on 17/4/23.
+ */
+public class SingleTableTest {
+}
--- src/test/resources/create_schema.sql
+++ src/test/resources/create_schema.sql
@@ -1,5 +1,6 @@
 DROP database if exists passport_0;
 DROP database if exists passport_1;
+DROP database if exists ext;
 
 CREATE SCHEMA `passport_0`;
 CREATE TABLE `passport_0`.`user_0` (
@@ -34,4 +35,11 @@ CREATE TABLE `passport_1`.`user_1` (
   `age` int(11) DEFAULT NULL,
   `role` int(11) DEFAULT NULL,
   UNIQUE KEY `id_UNIQUE` (`id`)
+);
+
+CREATE SCHEMA `ext`;
+CREATE TABLE `ext`.`language` (
+  `language_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(20) NOT NULL,
+  PRIMARY KEY (`language_id`)
 );
